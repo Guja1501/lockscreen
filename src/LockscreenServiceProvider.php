43a04097fadb4564f36b0c907b69e624d47ce578
Guja Babunashvili <Guja1501@gmail.com>
2017-09-09 20:10:15 +0400

    Updated config file

--- src/LockscreenServiceProvider.php
+++ src/LockscreenServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Rangoo\Lockscreen;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class LockscreenServiceProvider extends ServiceProvider {
@@ -20,15 +21,17 @@ class LockscreenServiceProvider extends ServiceProvider {
 	 * @return void
 	 */
 	public function register() {
-		$this->registerMiddleware();
-
-		$this->loadConfig();
+		$this
+			->registerMiddleware()
+			->loadConfig();
 	}
 
 	private function loadConfig() {
 		$this->publishes([
 			__DIR__ . '/config/lockscreen.php' => config_path('lockscreen.php')
 		], 'lockscreen-config');
+
+		return $this;
 	}
 
 	private function registerMiddleware() {
@@ -37,5 +40,7 @@ class LockscreenServiceProvider extends ServiceProvider {
 
 		$router->aliasMiddleware('auth.locked', Middleware\RedirectIfNotLocked::class);
 		$router->aliasMiddleware('auth.unlocked', Middleware\RedirectIfNotUnlocked::class);
+
+		return $this;
 	}
 }
--- src/config/lockscreen.php
+++ src/config/lockscreen.php
@@ -5,6 +5,13 @@ return [
 	'redirects' => [
 		'locked' => '/lockscreen',
 		'unlocked' => '/'
-	]
+	],
+
+	/*
+	 * Time before lockscreen
+	 * It's for javascript
+	 * null means turn off javascript
+	 */
+	'timeout' => 10
 
 ];
